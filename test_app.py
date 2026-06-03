9efba0d211d4bb8612aa0cfb1c51cb3792fd20b4
Abdallah Ahmed <142527303+AbdallahAhmed7@users.noreply.github.com>
2026-06-03 14:31:07 +0300

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,19 +1,13 @@
-
-
 import pytest
 from app import app
-
 
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-
-    with app.test_client() as client: 
+    with app.test_client() as client:
         yield client
 
-
-
-def test_hello(client):
+def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
-    assert b'Hello, World!' in response.data
+    assert b'CYBER CLOCK' in response.data
